5cafac68217db0e465ceda9a50488317c25b259c
brevins2 <kiggundubrevin@gmail.com>
2024-05-06 22:22:53 +0300

    new design

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { SafeAreaView, StyleSheet, Text, Image, Button, Alert, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, Image, Button, Alert, View, TouchableOpacity, TextInput } from 'react-native';
 import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
+import { useState } from 'react';
 
 export default function App() {
-  console.log('====================================');
-  console.log(useDeviceOrientation());
-  console.log('====================================');
+  const [ count, setCount ] = useState(false);
   return (
     <SafeAreaView>
       <View style={{
         backgroundColor: 'dodgerblue',
-        width: '50%',
-        height: 300,
+        width: '100%',
+        height: '100%',
         alignItems: 'center',
         justifyContent: 'center',
       }}>
+        <Text style={styles.heading}>Welcome To DoneWithIt</Text>
+        <Text style={{ color: 'white', fontSize: 20, }}>Number of counts {count}</Text>
+        <TextInput style={{ borderWidth: 2, padding: 3, paddingLeft: 10, borderRadius: 8, borderColor: 'white', color: 'white', width: 200}} placeholder='Enter name' keyboardType='numeric'></TextInput>
+        <TouchableOpacity style={styles.button} onPress={() => setCount(true)}>
+          <Text style={styles.buttonText}>Done</Text>
+        </TouchableOpacity>
 
+        <View style={styles.backpopup}></View>
       </View>
     </SafeAreaView>
   );
@@ -27,4 +33,42 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  heading: {
+    color: 'white',
+    fontSize: 30,
+    width: 250,
+    textAlign: 'center',
+    lineHeight: 30
+  },
+  button: {
+    backgroundColor: 'white',
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 4,
+    marginTop: 16,
+  },
+  buttonText: {
+    color: 'dodgerblue',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  backpopup: {
+    backgroundColor: 'rgba(0, 0, 0, 0.3)',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    padding: 24,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  popup: {
+    backgroundColor: 'white',
+    padding: 24,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
